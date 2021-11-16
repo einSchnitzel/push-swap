48c295c5bd53539d1657802f0784ad6adf921777
Sara Metzler <smetzler@student.42heilbronn.de>
2021-11-16 16:33:31 +0100

    make and header files

--- Makefile
+++ Makefile
@@ -6,31 +6,30 @@
 #    By: smetzler <smetzler@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/07 15:58:08 by smetzler          #+#    #+#              #
-#    Updated: 2021/11/07 16:11:26 by smetzler         ###   ########.fr        #
+#    Updated: 2021/11/16 16:28:20 by smetzler         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 CC = gcc
 NAME = push_swap
 FLAGS = -Werror -Wextra -Wall
-SRC=
+OPPATH = src/operations
 
-SRC=
-INCLUDE=
+SRC = src/main.c src/inputcheckers.c src/libftutils.c src/makelst.c src/free.c \
+		$(OPPATH)/push_op.c $(OPPATH)/rotate_op.c $(OPPATH)/rrotate_op.c\
+		#$(OPPATH)/swap_op.c
 
-${NAME}:
-	${CC} ${CFLAGS} -c ${SRC}
-	ar rc ${NAME} *.o
+$(NAME):	
+	$(CC) $(FLAGS) -o $(NAME) $(SRC)
 
 all:
-	${NAME} ./libft/libft.a
+	${NAME}
 
 clean:
-	@make -C libft/ clean
-	@echo ""
+	rm -f *.o
 
 fclean: clean
-	/bin/rm -f $(NAME)
-	@make -C libft/ fclean
+	rm -f $(NAME)
 
-re: fclean all
+re: fclean
+	$(CC) $(FLAGS) -o $(NAME) $(SRC)
--- pushswap.h
+++ pushswap.h
@@ -6,7 +6,7 @@
 /*   By: smetzler <smetzler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 16:28:16 by smetzler          #+#    #+#             */
-/*   Updated: 2021/11/10 13:43:31 by smetzler         ###   ########.fr       */
+/*   Updated: 2021/11/16 16:07:18 by smetzler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,22 @@ typedef struct s_node
 	struct s_node	*next;
 }	t_node;
 
-int		ft_atoi(const char *str);
+void	ft_pushtob(t_node **a, t_node **b);
+void	ft_pushtoa(t_node **a, t_node **b);
+void	ft_lstclear(t_node **lst);
+void	ft_lstdelone(t_node *lst);
+void	ft_free(void **arr);
+long	ft_atoi(const char *str);
 int		ft_isdigit(int n);
-t_node	*ft_makenode(long content);
+int		ft_putendl_fd(char *s, int fd);
+t_node	*ft_makenode(char *str, char **argv, int nvalues);
+int		ft_checkissorted(t_node **stack_a, int i);
 int		ft_checkint(long num);
+t_node	*ft_lstlast(t_node *lst);
 void	ft_lstadd_front(t_node **lst, t_node *new);
 void	ft_lstadd_back(t_node **lst, t_node *new);
 int		ft_argvalidcheck(char **argv, int nvalues);
-int		ft_checkers(char **argv, int nargs);
-t_node	ft_makestack(char **argv, int nvalues);
+int		ft_inputcheckers(char **argv, int nvalues, int i, int flag);
+t_node	*ft_makestack(char **argv, int nvalues);
+void	ft_freestacks(t_node **stack_a, t_node **stack_b);
 #endif
