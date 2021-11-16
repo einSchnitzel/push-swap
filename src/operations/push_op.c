b8fa22ba7f77c5b7133009487a551da0df4f0a1b
Sara Metzler <smetzler@student.42heilbronn.de>
2021-11-16 16:35:47 +0100

    push operation (error)

---
+++ src/operations/push_op.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_op.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smetzler <smetzler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/10 18:21:57 by smetzler          #+#    #+#             */
+/*   Updated: 2021/11/16 16:29:58 by smetzler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../pushswap.h"
+
+/*
+** Pushes top of b to a
+*/
+void	ft_pushtoa(t_node **a, t_node **b)
+{
+	t_node	*helper;
+
+	if (b == NULL)
+		return ;
+	helper = *b;
+	*b = (*b)->next;
+	helper->next = *a;
+	*a = helper;
+	write(1,"pa\n",3);
+}
+
+/*
+** Pushes top of a to b
+*/
+void	ft_pushtob(t_node **a, t_node **b)
+{
+	t_node *helper;
+
+	if (a == NULL)
+		return ;
+	helper = *a;
+	*a = (*a)->next;
+	helper->next = *b;
+	*b = helper;
+	write(1,"pb\n",3);
+}
